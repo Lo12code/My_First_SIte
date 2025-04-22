628c5ba27e0063ba305774e95eab7fa7fb380998
ubunto <lorenzodr95@hotmail.com>
2025-04-22 15:36:05 -0300

    video 6.2

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import RoomJoinPage from './RoomJoinPage';
+import Room from "./Room";
 import CreateRoomPage from './CreateRoomPage';
 import {BrowserRouter as Router, Routes, Route, Link, Redirect} from 'react-router-dom';
 
@@ -15,6 +16,7 @@ export default class HomePage extends Component{
                     <Route exact path='/' element={<p>This is the homepage</p>} />
                     <Route path='/join' element={<RoomJoinPage />} />
                     <Route path='/create' element={<CreateRoomPage />} />
+                    <Route path='/room/:roomCode' element={<Room />} />
                 </Routes>
             </Router>
         );
---
+++ frontend/src/components/Room.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+import { useParams } from "react-router-dom";
+
+
+class Room extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            votesToSkip: 2,
+            guestCanPause: false,
+            isHost: false,
+        };
+    }
+
+    render(){
+        return (<div>
+            <h3>Room code: {this.props.roomCode}</h3>
+            <p>Votes: {this.state.votesToSkip}</p>
+            <p>Guest can pause: {this.state.guestCanPause.toString()}</p>
+            <p>Host: {this.state.isHost.toString()}</p>
+        </div>);
+    }
+
+};
+
+const RoomWrapper = () => {
+    const { roomCode } = useParams();
+    return <Room roomCode={roomCode} />;
+};
+  
+export default RoomWrapper;
