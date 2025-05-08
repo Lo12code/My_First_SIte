7f493bbbd7eb1f99d3f58ddf59bf655f81ee7f82
ubunto <lorenzodr95@hotmail.com>
2025-05-07 21:05:28 -0300

    video 8

--- api/views.py
+++ api/views.py
@@ -26,7 +26,6 @@ class GetRoomView(APIView):
             return Response({'Room not found': 'Invalid room code.'}, status=status.HTTP_404_NOT_FOUND)
         return Response({'Bad request': 'Code parameter not found.'}, status=status.HTTP_400_BAD_REQUEST)
 
-
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
 
@@ -45,9 +44,29 @@ class CreateRoomView(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
                 room.save()
-        
-        return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
+
+class JoinRoomView(APIView):
+    lookup_url_kwarg = 'code'
+    
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        
+        code = request.data.get(self.lookup_url_kwarg)
+        if code != None:
+            room_result = Room.objects.filter(code=code)
+            
+            if len(room_result) > 0:
+                room = room_result[0]
+                self.request.session['room_code'] = code
+                return Response({'message': 'Room Joined.'}, status=status.HTTP_200_OK)
+            return Response({'Bad Rquest': 'Invalid room code.'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'Bad Rquest': 'Invalid post data, did not find a code key.'}, status=status.HTTP_400_BAD_REQUEST)
--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,12 +1,86 @@
 import React, {Component} from 'react';
+import {
+    TextField,
+    Button,
+    Grid,
+    Typography
+} from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 
-export default class RoomJoinPage extends Component{
+
+class RoomJoinPage extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            roomCode: '',
+            error: ''
+        }
+
+        this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+        this.roomButtonPressed = this.roomButtonPressed.bind(this);
     }
 
     render(){
-        return <p>This is the room join page.</p>;
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align='center'>
+                    <Typography variant='h4' component='h4'>
+                        Join a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align='center'>
+                    <TextField 
+                        error={this.state.error}
+                        label='Code'
+                        placeholder='Enter a room code'
+                        value={this.state.roomCode}
+                        helperText={this.state.error}
+                        variant='outlined'
+                        onChange={this.handleTextFieldChange}>
+                    </TextField>
+                </Grid>
+                <Grid item xs={12} align='center'>
+                    <Button variant='contained' color='primary' onClick={this.roomButtonPressed} element={Link}>Enter Room</Button>
+                </Grid>
+                <Grid item xs={12} align='center'>
+                <Button variant='contained' color='secondary' to='/' component={Link}>Back</Button>
+                </Grid>
+            </Grid>
+        );
     }
 
+    handleTextFieldChange(e) {
+        this.setState({
+            roomCode: e.target.value
+        });
+    }
+    
+    roomButtonPressed(e){
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                code: this.state.roomCode,
+            })
+        };
+        fetch('/api/join-room', requestOptions)
+            .then((response) => {
+                if (response.ok){
+                    this.props.navigate(`/room/${this.state.roomCode}`)
+                }else{
+                    this.setState({error: 'Room not found.'})
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
 }
+
+
+const RoomJoinPageWrapper = () => {
+    const navigate = useNavigate();
+    return <RoomJoinPage navigate={navigate} />;
+};
+
+export default RoomJoinPageWrapper;
