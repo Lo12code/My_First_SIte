55fe7922b29dfff640feaa4680c48920ff5e0f9a
ubunto <lorenzodr95@hotmail.com>
2025-06-05 21:07:44 -0300

    Fixing dependencies problems

--- reacjdjangoapp/settings.py
+++ reacjdjangoapp/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_PYTHON_HOST'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
         'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
