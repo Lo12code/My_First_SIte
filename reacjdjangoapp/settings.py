94a0d0c8f4f0d844e6b54bdc2b618cde9ce442c9
ubunto <lorenzodr95@hotmail.com>
2025-06-11 16:31:33 -0300

    starting to get whats wrong

--- reacjdjangoapp/settings.py
+++ reacjdjangoapp/settings.py
@@ -130,6 +130,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(os.path.join(BASE_DIR, 'frontend'), 'static'),
+    "/var/www/static/",
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
