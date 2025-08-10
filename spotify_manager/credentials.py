4054a84088fefeeba18bae5f25d1df3263463953
ubunto <lorenzodr95@hotmail.com>
2025-08-10 20:26:21 -0300

    Creating spotify app

---
+++ spotify_manager/credentials.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SPOTIFY_DOTENV_PATH = os.path.join(BASE_DIR, '.env.spotify')
+load_dotenv(dotenv_path=SPOTIFY_DOTENV_PATH)
+
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+REDIRECT_URL = ''
