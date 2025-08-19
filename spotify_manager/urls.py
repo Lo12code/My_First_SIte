39c8c8cc4b352ac012711e4a6aec3c72f26baac7
ubunto <lorenzodr95@hotmail.com>
2025-08-19 13:49:26 -0300

    Creating backend endpoints for button play/pause

--- spotify_manager/urls.py
+++ spotify_manager/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import AuthUrl, SpotifyCallback, IsAuthenticated, CurrentSong
+from .views import AuthUrl, \
+    SpotifyCallback, \
+    IsAuthenticated, \
+    CurrentSong, \
+    PauseSong, \
+    PlaySong
 
 urlpatterns = [
     path('get-auth-url', AuthUrl.as_view()),
     path('redirect', SpotifyCallback.as_view()),
     path('is-authenticated', IsAuthenticated.as_view()),
     path('current-song', CurrentSong.as_view()),
+    path('pause', PauseSong.as_view()),
+    path('play', PlaySong.as_view())
 ]
--- spotify_manager/views.py
+++ spotify_manager/views.py
@@ -7,7 +7,11 @@ from requests import Request, post
 
 from api.models import Room
 from .credentials import REDIRECT_URL, CLIENT_ID, CLIENT_SECRET
-from .utils import update_or_create_user_token, is_spotify_authenticated, execute_spotify_api_request
+from .utils import update_or_create_user_token, \
+    is_spotify_authenticated, \
+    execute_spotify_api_request \
+    play_song, \
+    pause_song
 
 
 class AuthUrl(APIView):
@@ -113,3 +117,24 @@ class CurrentSong(APIView):
         }
 
         return Response(song, status=status.HTTP_200_OK)
+
+
+class PauseSong(APIView):
+    def put(self, response, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)[0]
+        if self.request.session.session_key == room.host or room.guest_can_pause:
+            pause_song(room.host)
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        
+        return Response({}, status=status.HTTP_403_FORBIDDEN)
+    
+class PlaySong(APIView):
+    def put(self, response, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)[0]
+        if self.request.session.session_key == room.host or room.guest_can_pause:
+            play_song(room.host)
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        
+        return Response({}, status=status.HTTP_403_FORBIDDEN)
