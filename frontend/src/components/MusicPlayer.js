a11bbb0d0a1cb62c24faa813aea0efc10843690e
Lo12code <lorenzodr95@hotmail.com>
2025-12-24 17:36:19 -0300

    Fixing music player

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {
+  Box,
   Grid,
   Typography,
   Card,
@@ -47,25 +48,35 @@ export default class MusicPlayer extends Component {
 
     return (
       <Card>
-        <Grid container alignItems="center">
-          <Grid item align="center" size={12}>
-            <img src={this.props.image_url} height="100%" width="100%" />
+        <Grid container height="600px">
+          <Grid item justifyContent="flex-start" alignItems="center" xs={6}>
+              {this.props.image_url ? (
+                <img src={this.props.image_url} height="100%" width="100%" />
+              ) : (
+                <Box width="100%" height="100%" bgcolor="#eee" />
+              )}
           </Grid>
-          <Grid item align="center" size={8}>
-            <Typography component="h5" variant="h5">
-              {this.props.title}
-            </Typography>
-            <Typography color="textSecondary" variant="subtitle1">
-              {this.props.artist}
-            </Typography>
-            <IconButton onClick={() => {
-              this.props.is_playing ? this.pauseSong() : this.playSong();
-            }}>
-              {this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
-            </IconButton>
-            <IconButton onClick={() => this.skipSong()}>
-              <SkipNextIcon /> {this.props.votes} / {this.props.votes_required}
-            </IconButton>
+          <Grid item xs={6} container direction="column" alignItems="center" justifyContent="center" spacing={1}>
+            <Grid item>
+              <Typography component="h5" variant="h5">
+                {this.props.title ? this.props.title : "No music playing"}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography color="textSecondary" variant="subtitle1">
+                {this.props.artist ? this.props.artist : "No music playing"}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <IconButton onClick={() => {
+                this.props.is_playing ? this.pauseSong() : this.playSong();
+              }}>
+                {this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
+              </IconButton>
+              <IconButton onClick={() => this.skipSong()}>
+                <SkipNextIcon /> {this.props.votes} / {this.props.votes_required}
+              </IconButton>
+            </Grid>
           </Grid>
         </Grid>
         <LinearProgress variant="determinate" value={songProgress} />
